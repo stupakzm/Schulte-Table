420cef66dcfcf911fa3c687b0fb2e92e55b79d8b
stupakzm <85805156+stupakzm@users.noreply.github.com>
2024-02-23 21:05:26 +0100

    fixed errors with Statistics

--- Settings.cs
+++ Settings.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.Rendering;
 
 public class Settings : MonoBehaviour {
     [SerializeField] private TextMeshProUGUI statsText;
     [SerializeField] private GameObject[] selectedLightMode;
 
+
+    private void Start() {
+    }
     public void ShowSelectedLightMode() {
         if (PlayerPrefs.GetString(Const.LIGHT_SETTINGS) == Const.LIGHT_MODE) {
             selectedLightMode[0].SetActive(true);
@@ -33,8 +37,10 @@ public class Settings : MonoBehaviour {
     public void UpdateStatsText() {
         statsText.text = string.Empty;
         for (int i = 0; i < 6; i++) {
-            statsText.text += "Best time in " + (Mode)i + " mode is " + Statistics.GetBestTime((Mode)i) +
-                ".\nAttempts - " + Statistics.GetAttemptCount((Mode)i) + "\n";
+            string bestTime = Statistics.GetBestTime((Mode)i) == float.MaxValue ? "NaN" : Statistics.GetBestTime((Mode)i).ToString("#.0");
+            string attemptCount = Statistics.GetAttemptCount((Mode)i).ToString();
+            statsText.text += "Best time in " + (Mode)i + " mode is " + bestTime +
+                ".\nAttempts - " + attemptCount + "\n";
         }
     }
 }
--- Statistics.cs
+++ Statistics.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public static class Statistics {
 
+
     public static void SetBestTime(Mode mode, float time) {
-        if (PlayerPrefs.GetFloat(mode.ToString() + Const.BEST_TIME, float.MaxValue) > time) {
+        if (PlayerPrefs.GetFloat(mode.ToString() + Const.BEST_TIME, float.MaxValue) > time | (PlayerPrefs.GetFloat(mode.ToString() + Const.BEST_TIME)/2) < 1) {
             PlayerPrefs.SetFloat(mode.ToString() + Const.BEST_TIME, time);
         }
     }
 
+    public static void SetBestTimeNoCondition(Mode mode, float time) {
+            PlayerPrefs.SetFloat(mode.ToString() + Const.BEST_TIME, time);
+        
+    }
+
+    public static void ResetBestTime(Mode mode) {
+        PlayerPrefs.DeleteKey(mode.ToString() + Const.BEST_TIME);
+    }
+
     public static float GetBestTime(Mode mode) {
-        return PlayerPrefs.GetFloat(mode.ToString() + Const.BEST_TIME);
+        return PlayerPrefs.GetFloat(mode.ToString() + Const.BEST_TIME, float.MaxValue);
     }
 
     public static void SetAttemptCount(Mode mode) {
